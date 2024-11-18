57f8d1e2d9e4a50db4449c9608041b0d425a764d
Oliver Japp <114418435+OliverJapp@users.noreply.github.com>
2024-11-18 23:23:42 +0000

    First version of World
    Next step is to allow robot to navigate to custom point

--- World/World.py
+++ World/World.py
@@ -1,22 +1,89 @@
+#!/usr/bin/env python3
+
+"""
+Example showing how to build a world and use it with pyrobosim,
+additionally starting up a ROS interface.
+"""
 import os
+from pyrobosim.utils import search_graph
+import rclpy
+import threading
 import numpy as np
-from pyrobosim.core import World
-from pyrobosim.gui.main import start_gui
-from pyrobosim.core.yaml_utils import WorldYamlLoader
+
+from pyrobosim.core import Robot, World, WorldYamlLoader
+from pyrobosim.navigation import ConstantVelocityExecutor, RRTPlanner
+from pyrobosim.gui import start_gui
 from pyrobosim.utils.general import get_data_folder
 from pyrobosim.utils.pose import Pose
+from WarehousePathing import Graph 
+import pyrobosim
 
-# Then, you can add the other entities
+
 data_folder = get_data_folder()
 
-def create_world_from_yaml(world_file):
-    return WorldYamlLoader().from_file(os.path.join(data_folder, world_file))
+
+def create_world():
+    """Create a test world"""
+    world = World()
+    world.set_metadata(
+        locations=os.path.join("/home/slane/pyrobosim/World/Location.yaml"),
+    )
+    Width = 2
+    Height = 2
+
+
+
+    # Set the location and object metadata
+    # Add rooms
+    #graph = world.graph_node_from_entity(Graph(Height, Width))
+    r1coords = [(-Width*2, 0),
+                (0, 0),
+                (0, Height*2),
+                (-Width*2, Height*2)]
+    world.add_room(
+        name="Warehouse", footprint=r1coords, wall_width=0.1, color=[0, 0, 0]
+    )
+
+    for j in range(Height*2):
+        for i in range(Width*2):
+                world.add_location(
+                    category="shelf", parent="Warehouse", pose=Pose(x=-i, y=j, yaw=-np.pi / 1.0), color = [0, 0, 0]
+                )
+
+    planner_config = {
+        "world": world,
+        "bidirectional": True,
+        "rrt_connect": False,
+        "rrt_star": True,
+        "collision_check_step_dist": 0.025,
+        "max_connection_dist": 0.5,
+        "rewire_radius": 1.5,
+        "compress_path": False,
+    }            
+    path_planner = RRTPlanner(**planner_config)
+    robot = Robot(
+        name="Taxi_bot",
+        radius=0.1,
+        color = [1, 0, 0],
+        initial_battery_level = float("inf"),
+        path_executor=ConstantVelocityExecutor(),
+        path_planner=path_planner,
+    )
+    world.add_robot(robot, loc="Warehouse", pose=Pose(x=-0.25, y=0.25, yaw=-np.pi / 1.0))
+    return world
+
+
+def create_ros_node():
+    """Initializes ROS node"""
+    rclpy.init()
+    # Set the world
+    world = create_world()
+
+    return world
+
 
 if __name__ == "__main__":
-    world = World()
-    world = create_world_from_yaml("/home/slane/pyrobosim/World/World_data.yaml")
-
-    # Start ROS node in separate thread
+    world = create_ros_node()
 
     # Start GUI in main thread
     start_gui(world)
