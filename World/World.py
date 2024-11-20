5d1e5fc909aafef328ea4bd8cd76c63520d9d1f1
Oliver Japp <114418435+OliverJapp@users.noreply.github.com>
2024-11-20 12:35:46 +0000

    Pyrobosim base commands works

--- World/World.py
+++ World/World.py
@@ -5,11 +5,13 @@ Example showing how to build a world and use it with pyrobosim,
 additionally starting up a ROS interface.
 """
 import os
+import random
+from pyrobosim.planning.actions import TaskPlan
 from pyrobosim.utils import search_graph
 import rclpy
 import threading
 import numpy as np
-
+from pyrobosim.planning.actions import TaskAction
 from pyrobosim.core import Robot, World, WorldYamlLoader
 from pyrobosim.navigation import ConstantVelocityExecutor, RRTPlanner
 from pyrobosim.gui import start_gui
@@ -27,45 +29,63 @@ def create_world():
     world = World()
     world.set_metadata(
         locations=os.path.join("/home/slane/pyrobosim/World/Location.yaml"),
+        objects=os.path.join("/home/slane/pyrobosim/World/Object.yaml"),
     )
-    Width = 20
-    Height = 20
+    Width = 10
+    Height = 10
 
 
 
     # Set the location and object metadata
     # Add rooms
     #graph = world.graph_node_from_entity(Graph(Height, Width))
-    r1coords = [(-20, 0),
-                (0, 0),
-                (0, 20),
-                (-20, 20)]
+    r1coords = [(-Width, 0),
+                (2, 0),
+                (2, Width),
+                (-Width, Height)]
+
+    r2coords = [(2, 0),
+                (5, 0),
+                (5, 5),
+                (2, 5)]
+    
+    r3coords = [(2, Height-5),
+                (5, Height-5),
+                (5, Height),
+                (2, Height)]
+                
+    Warehouse = world.add_room(
+                name="Warehouse", footprint=r1coords, wall_width=0.1, color=[0, 0, 0]
+            )
+    
+    
+    
+    dif = 3
+    Nav_points = []
+    Shelves = []
+
+    Nav_points.append(world.add_location(
+                category="Nav_point", parent="Warehouse", pose=Pose(x=-0.5, y=0.5, yaw=-np.pi / 1.0), color = [0, 0, 0]
+            ))
+    
+    Nav_points.append(world.add_location(
+                category="Nav_point", parent="Warehouse", pose=Pose(x=-0.5, y=Height-3.5, yaw=-np.pi / 1.0), color = [0, 0, 0]
+            ))
+    
+    Shelves.append(world.add_location(
+                category="shelf", parent="Warehouse", pose=Pose(x=1, y=0.5, yaw=-np.pi / 1.0), color = [0, 0, 0]
+            ))
     
 
-    world.add_room(
-        name="Warehouse", footprint=r1coords, wall_width=0.1, color=[0, 0, 0]
-    )
-
-    world.add_location(
-        category="shelf", parent="Warehouse", pose=Pose(x=-3, y=3, yaw=-np.pi / 1.0), color = [0, 0, 0]
-    )
-
-    world.add_location(
-        category="Nav_point", parent="Warehouse", pose=Pose(x=-5.5, y=1, yaw=-np.pi / 1.0), color = [0, 0, 0]
-    )
-
-    world.add_location(
-        category="shelf", parent="Warehouse", pose=Pose(x=-8, y=3, yaw=-np.pi / 1.0), color = [0, 0, 0]
-    )
-    world.add_location(
-        category="shelf", parent="Warehouse", pose=Pose(x=-3, y=8, yaw=-np.pi / 1.0), color = [0, 0, 0]
-    )
-    world.add_location(
-        category="shelf", parent="Warehouse", pose=Pose(x=-8, y=8, yaw=-np.pi / 1.0), color = [0, 0, 0]
-    )
-
-            
-
+    for i in range(2, Width, dif):
+        for j in range(2, Height, dif):
+            Shelves.append(world.add_location(
+                category="shelf", parent="Warehouse", pose=Pose(x=-i, y=j, yaw=-np.pi / 1.0), color = [0, 0, 0]
+            ))
+            Nav_points.append(world.add_location(
+                category="Nav_point", parent="Warehouse", pose=Pose(x=-i-dif/2, y=j-dif/2, yaw=-np.pi / 1.0), color = [0, 0, 0]
+            ))
+        
 
     planner_config = {
         "world": world,
@@ -73,20 +93,41 @@ def create_world():
         "rrt_connect": False,
         "rrt_star": True,
         "collision_check_step_dist": 0.025,
-        "max_connection_dist": 0.5,
+        "max_connection_dist": 1.5,
         "rewire_radius": 1.5,
         "compress_path": False,
     }            
     path_planner = RRTPlanner(**planner_config)
-    robot = Robot(
-        name="Taxi_bot",
-        radius=0.1,
-        color = [1, 0, 0],
-        initial_battery_level = float("inf"),
-        path_executor=ConstantVelocityExecutor(),
-        path_planner=path_planner,
-    )
-    world.add_robot(robot, loc="Warehouse", pose=Pose(x=-0.25, y=0.25, yaw=-np.pi / 1.0))
+    robots = []    
+    
+    #for i in range(Width//dif):
+
+    robots.append(Robot(name="Taxi_bot", radius=0.1, color = [1, 0, 0], initial_battery_level = float("inf"), path_executor=ConstantVelocityExecutor(), path_planner=path_planner,))
+        #robots[i].name = f"Taxi_bot{i}"
+    world.add_robot(robots[0], loc="Warehouse", pose=Pose(x=-0.5, y=1, yaw=-np.pi / 1.0))
+    world.add_object(category="banana", parent=Shelves[0], pose=Pose(x=1, y=0.5, yaw=0.0))
+
+    
+    actions = [
+    TaskAction(
+        "navigate",
+        source_location="Warehouse",
+        target_location="shelf0",
+    ),
+    TaskAction("detect", object="banana"),
+    TaskAction("pick", object="banana"),
+    TaskAction(
+        "navigate",
+        source_location="shelf0",
+        target_location="shelf8",
+    ),
+    TaskAction("place", object="banana"),
+    ]
+
+    plan = TaskPlan(actions=actions)
+    result, num_completed = robots[0].execute_plan(plan)
+
+
     return world
 
 
